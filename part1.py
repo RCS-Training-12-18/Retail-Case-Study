7dba3afce2ed0c666fe402c6b7eb486c4e51a11d
Michael Rose <mikescottrose@gmail.com>
2018-12-21 16:08:23 -0800

    Refactored the code
    Refactored the code and tried to make it more reusable

--- part1.py
+++ part1.py
@@ -38,6 +38,7 @@ rem_loc = False
 if rem_loc:
     import shutil
 
+
 # Just to show the start of the program in the middle of all the output
 def prog_start():
     print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
@@ -59,79 +60,73 @@ def load_dfs():
     return p_df, s97_df, s98_df, s98d_df
 
 
-def get_aws_creds():
-    f = open(cred_file, 'r')
-    return f.read().split(",")
+def load_df(table_name, incremental,ts):
+    sqlContext = SQLContext(sc)
+    df = sqlContext.read.format("jdbc").option("url", url).option("driver", "com.mysql.jdbc.Driver").option(
+          "dbtable", table_name).option("user", u).option("password", pw).load()
+    if incremental == 'I':
+        return df.filter(df.last_update > ts)
+    else:
+        return df
 
+
+def write_df(df, table_name, w_time, incremental):
+    df.write.mode('append').format("avro").save(
+        raw_out_loc + table_name + "/" + w_time + str(incremental))
+
+
+def avro_to_s3(dir_name, incremental, write_time):
+  client = boto3.client('s3')
+  for f in os.listdir(raw_out_loc[7:] + dir_name + "/" + write_time + str(incremental)):
+      if f.startswith('part'):
+          out = raw_out_loc[7:] + dir_name + "/" + write_time + str(incremental) + "/" + f
+  client.put_object(Bucket=bucket_name, Key="raw/" + dir_name + "/" + write_time + str(incremental) + ".arvo",
+                    Body=open(out, 'r'))
 
 def main(arg):
     # If no arguments in command line, exit the program with an error
     if len(arg) < 1:
         print "No command line arguments."
         exit(1)
+    if arg[0] != 'I' and arg[0] != 'F':
+        print "Bad argument. Use 'I' or 'F'"
+        exit(1)
     # Try to read the unix timestamp from the file listed in the variable "last_update"
     try:
         f = open(last_update, 'r')
         last_update_unix_ts = f.read().rstrip()
         f.close()
-        last_up = True
     except:
-        last_up = False
+        if arg[0] == 'I':
+            print "Failed to load file:" + last_update
+            exit(1)
+        last_update_unix_ts = 0
     # Used when updating the "last_update" file
     new_update_time = int(time.time())
-    if last_up and arg[0] == 'I':
-        # Calls load_dfs() to set the unfiltered dataframes
-        prom_df, sales97_df, sales98_df, sales98_dec_df = load_dfs()
-        prom_out_df = prom_df.filter(prom_df.last_update > last_update_unix_ts)
-        sales97_out_df = sales97_df.filter(sales97_df.last_update > last_update_unix_ts)
-        sales98_out_df = sales98_df.filter(sales98_df.last_update > last_update_unix_ts)
-        sales98_dec_out_df = sales98_dec_df.filter(sales98_dec_df.last_update > last_update_unix_ts)
-    elif arg[0] == 'F':
-        # Calls load_dfs() to set the dataframes that will be written. No filtering necessary
-        prom_out_df, sales97_out_df, sales98_out_df, sales98_dec_out_df = load_dfs()
-    else:
-        # Exit because command line input was not able to be parsed
-        print "Run failed. Either file: " + last_update + " doesn't exist and an incremental load was attempted or" + \
-            "neither F or I was used for the argument."
-        exit(1)
+    prom_df = load_df("promotion", arg[0],last_update_unix_ts)
+    s97_df = load_df("sales_fact_1997",arg[0],last_update_unix_ts)
+    s98_df = load_df("sales_fact_1998",arg[0],last_update_unix_ts)
+    s98d_df = load_df("sales_fact_dec_1998",arg[0],last_update_unix_ts)
     # Saves the dataframes as avro files
     write_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    prom_out_df.write.mode('append').format("avro").save(
-        raw_out_loc + "promotion/" + write_time + str(arg[0]))
-    sales97_out_df.write.mode('append').format("avro").save(
-        raw_out_loc + "97/" + write_time + str(arg[0]))
-    sales98_out_df.write.mode('append').format("avro").save(
-        raw_out_loc + "98/" + write_time + str(arg[0]))
-    sales98_dec_out_df.write.mode('append').format("avro").save(
-        raw_out_loc + "98_dec/" + write_time + str(arg[0]))
+    write_df(prom_df, "promotion", write_time, arg[0])
+    write_df(s97_df, "s97", write_time, arg[0])
+    write_df(s98_df, "s98", write_time, arg[0])
+    write_df(s98d_df, "s98d", write_time, arg[0])
+
     # Update the "last_update" file with the newest runtime.
     f = open(last_update, 'w+')
     f.write(str(new_update_time))
     f.close()
     if len(arg) >= 2 and (arg[1] == "s3" or arg[1] == "S3"):
-        client = boto3.client('s3')
-        for f in os.listdir(raw_out_loc[7:] + "promotion/" + write_time + str(arg[0])):
-            if f.startswith('part'):
-                prom_out = raw_out_loc[7:] + "promotion/" + write_time + str(arg[0]) + "/" + f
-        client.put_object(Bucket = bucket_name, Key = "raw/promotion/"+write_time+str(arg[0])+".arvo",
-                          Body = open(prom_out, 'r'))
-        for f in os.listdir(raw_out_loc[7:] + "97/" + write_time + str(arg[0])):
-            if f.startswith('part'):
-                f97_out = raw_out_loc[7:] + "97/" + write_time + str(arg[0]) + "/" + f
-        client.put_object(Bucket = bucket_name, Key = "raw/97/"+write_time+str(arg[0])+".arvo",
-                          Body = open(f97_out, 'r'))
-        for f in os.listdir(raw_out_loc[7:] + "98/" + write_time + str(arg[0])):
-            if f.startswith('part'):
-                f98_out = raw_out_loc[7:] + "98/" + write_time + str(arg[0]) + "/" + f
-        client.put_object(Bucket = bucket_name, Key = "raw/98/"+write_time+str(arg[0])+".arvo",
-                          Body = open(f98_out, 'r'))
-        for f in os.listdir(raw_out_loc[7:] + "98_dec/" + write_time + str(arg[0])):
-            if f.startswith('part'):
-                f98d_out = raw_out_loc[7:] + "98_dec/" + write_time + str(arg[0]) + "/" + f
-        client.put_object(Bucket = bucket_name, Key = "raw/98d/"+write_time+str(arg[0])+".arvo",
-                          Body = open(f98d_out, 'r'))
+        avro_to_s3("promotion", arg[0], write_time)
+        avro_to_s3("s97", arg[0], write_time)
+        avro_to_s3("s98", arg[0], write_time)
+        avro_to_s3("s98d", arg[0], write_time)
     if rem_loc:
         shutil.rmtree(raw_out_loc[7:])
+
+
 # Runs the script
 if __name__ == "__main__":
     prog_start()
