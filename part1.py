cc93107c674ef2620ab6f89ad7cdbc383c9331a8
Michael Rose <mikescottrose@gmail.com>
2018-12-23 22:46:11 -0800

    Removed unnessary globals and refactored
    Removed a creds file location.
    Refactored the table names into a file so that it could be changed without changing the code. (This will later be stored on S3)
    Changed prog_start to be used for headers for each code section.
    Refactored the reading from MySQL and writing to S3/Disk so that it used an array of table names rather than hardcoding each one in.

--- part1.py
+++ part1.py
@@ -8,7 +8,6 @@
 # To run the script with S3 pushing run
 # spark-submit --packages mysql:mysql-connector-java:5.1.39,org.apache.spark:spark-avro_2.11:2.4.0 part1.py I s3
 
-
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession, SQLContext
@@ -20,25 +19,36 @@ import os
 import tempfile
 
 #Change these. Later will use a config file
-cred_file = "creds"
+tables = "tables"
 u = "user"
 pw = "password"
 url = "jdbc:mysql://localhost/foodmart"
 #File to save last update time, will move this to S3 later
-last_update = "case-study/last_update-p1"
+last_update = "last_update-p1"
 raw_out_loc = "file:///home/msr/case-study/raw/"
 bucket_name = "rcs-training-12-18"
 
 sc = SparkContext("local[2]", "Case-Study-Part-1")
 
 
-# Just to show the start of the program in the middle of all the output
-def prog_start():
-    print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-    print "----------PROGRAM START----------"
-    print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+# Just to show the each section of the program in the middle of all the output
+def section_header(h):
+    print "\n\n\n"
+    print "----------"+h+"----------"
+    print "\n\n\n"
 
 
+# Load in table names into an array that is returned
+def table_names():
+    t_names = []
+    f = open(tables, 'r')
+    for l in f:
+        t_names.append(l.rstrip())
+    return t_names
+
+
+# Loads a dataframe and returns it
+# If the load is incremental, removes old data
 def load_df(table_name, incremental,ts):
     sqlContext = SQLContext(sc)
     df = sqlContext.read.format("jdbc").option("url", url).option("driver", "com.mysql.jdbc.Driver").option(
@@ -49,6 +59,8 @@ def load_df(table_name, incremental,ts):
         return df
 
 
+# Writes the dataframe to S3 using boto3
+# Saves the data as an avro
 def write_avro2s3(df, dir_name, write_time, incremental):
     client = boto3.client('s3')
     path = os.path.join(tempfile.mkdtemp(), dir_name)
@@ -56,8 +68,7 @@ def write_avro2s3(df, dir_name, write_time, incremental):
     for f in os.listdir(path):
         if f.startswith('part'):
             out = path + "/" + f
-    client.put_object(Bucket=bucket_name, Key="raw/" + dir_name + "/" +
-                                              write_time + str(incremental) + ".arvo",
+    client.put_object(Bucket=bucket_name, Key="raw/" + dir_name + "/" + write_time + str(incremental) + ".arvo",
                       Body=open(out, 'r'))
 
 
@@ -86,10 +97,12 @@ def main(arg):
         last_update_unix_ts = 0
     # Used when updating the "last_update" file
     new_update_time = int(time.time())
-    prom_df = load_df("promotion", arg[0],last_update_unix_ts)
-    s97_df = load_df("sales_fact_1997",arg[0],last_update_unix_ts)
-    s98_df = load_df("sales_fact_1998",arg[0],last_update_unix_ts)
-    s98d_df = load_df("sales_fact_dec_1998",arg[0],last_update_unix_ts)
+    dfs = []
+    t_names = table_names()
+    # Load the dataframes into a list
+    section_header("Load Dataframes")
+    for n in t_names:
+        dfs.append(load_df(n,arg[0],last_update_unix_ts))
     write_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     # Update the "last_update" file with the newest runtime.
     f = open(last_update, 'w+')
@@ -97,19 +110,17 @@ def main(arg):
     f.close()
     # Saves the dataframes as avro files in S3
     if len(arg) >= 2 and (arg[1] == "s3" or arg[1] == "S3"):
-        write_avro2s3(prom_df, "promotion", write_time, arg[0])
-        write_avro2s3(s97_df, "s97", write_time, arg[0])
-        write_avro2s3(s98_df, "s98", write_time, arg[0])
-        write_avro2s3(s98d_df, "s98d", write_time, arg[0])
+        section_header("Write to S3")
+        for i in range(len(t_names)):
+            write_avro2s3(dfs[i], t_names[i], write_time, arg[0])
     # Saves the dataframes as avro files locally
     else:
-        write_df(prom_df, "promotion", write_time, arg[0])
-        write_df(s97_df, "s97", write_time, arg[0])
-        write_df(s98_df, "s98", write_time, arg[0])
-        write_df(s98d_df, "s98d", write_time, arg[0])
+        section_header("Write Locally")
+        for i in range(len(t_names)):
+            write_df(dfs[i], t_names[i], write_time, arg[0])
 
 
 # Runs the script
 if __name__ == "__main__":
-    prog_start()
+    section_header("Program Start")
     main(sys.argv[1:])
