62500ff6a38196da0e7c0dc85d90c7ba04a7a528
Michael Rose <mikescottrose@gmail.com>
2019-01-02 14:03:55 -0800

    Updated the dag
    The names are more accurate and the interval was changed to 45 minutes.

--- fm-dag.py
+++ fm-dag.py
@@ -6,6 +6,7 @@ from airflow.utils.trigger_rule import TriggerRule
 from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.models import Variable
 
 ss = '/home/msr/spark-2.4.0-bin-hadoop2.7/bin/spark-submit'
 mysql_pkg = 'mysql:mysql-connector-java:5.1.39'
@@ -13,6 +14,7 @@ avro_pkg = 'org.apache.spark:spark-avro_2.11:2.4.0'
 p1pkg = '--packages ' + mysql_pkg + ',' + avro_pkg
 p2pkg = '--packages ' + avro_pkg
 py_file_loc = '/mnt/c/Users/MikeS/Documents/Github/Retail-Case-Study/'
+
 args = {
     'owner': 'airflow',
     'start_date': airflow.utils.dates.days_ago(2),
@@ -22,7 +24,7 @@ dag = DAG(
     dag_id='foodmart',
     default_args=args,
     catchup = False,
-    schedule_interval='* */2 * * *',
+    schedule_interval='*/45 * * * *',
     dagrun_timeout=timedelta(minutes=10),
 )
 
@@ -32,33 +34,33 @@ start = DummyOperator(
 )
 
 p1f = BashOperator(
-	task_id='part1_full',
+	task_id='import_from_mysql_to_s3_full_load',
 	bash_command=ss + " " + p1pkg + " "+ py_file_loc + "part1.py F",
 	trigger_rule=TriggerRule.ONE_FAILED,
 	dag=dag
 )
 
 p1i = BashOperator(
-        task_id='part1_inc',
+        task_id='import_from_mysql_to_s3_full_incremental',
         bash_command=ss + " " + p1pkg + " "+ py_file_loc + "part1.py I",
         dag=dag
 )
 
 p2 = BashOperator(
-        task_id='part2',
+        task_id='data_cleansing',
         bash_command=ss + " " + p2pkg + " "+ py_file_loc + "part2.py",
 	trigger_rule=TriggerRule.ONE_SUCCESS,
         dag=dag
 )
 
 p3 = BashOperator(
-        task_id='part3',
+        task_id='aggregation_and_move_to_staging',
         bash_command=ss + " " + py_file_loc + "part3.py",
         dag=dag
 )
 
 p4 = BashOperator(
-        task_id='part4',
+        task_id='move_data_from_s3_to_snowflake',
         bash_command=ss + " " + py_file_loc + "part4.py",
         dag=dag
 )
