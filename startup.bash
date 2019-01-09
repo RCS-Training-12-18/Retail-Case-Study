9cca2443afc06991845e33e6eb06455a7526cef1
Michael Rose <mikescottrose@gmail.com>
2019-01-09 11:41:04 -0800

    Looped the startup.bash
    Made it easier to add new tables or remove tables in the startup.bash file

--- startup.bash
+++ startup.bash
@@ -1,29 +1,12 @@
 #!/bin/bash
+tables='promotion sales_fact_1997 sales_fact_1998 sales_fact_dec_1998 store time_by_day'
+for table in $tables
+do
 mysql -u user -ppassword --force <<EOF
 use foodmart;
-alter table promotion add last_update INT;
-update promotion set last_update = UNIX_TIMESTAMP(NOW());
-alter table sales_fact_1997 add last_update INT;
-update sales_fact_1997 set last_update = UNIX_TIMESTAMP(NOW());
-alter table sales_fact_1998 add last_update INT;
-update sales_fact_1998 set last_update = UNIX_TIMESTAMP(NOW());
-alter table sales_fact_dec_1998 add last_update INT;
-update sales_fact_dec_1998 set last_update = UNIX_TIMESTAMP(NOW());
-alter table store add last_update INT;
-update store set last_update = UNIX_TIMESTAMP(NOW());
-alter table time_by_day add last_update INT;
-update time_by_day set last_update = UNIX_TIMESTAMP(NOW());
-create trigger sales_fact_1997_insert before insert on sales_fact_1997 for each row set new.last_update = unix_timestamp(now());
-create trigger sales_fact_1997_update before update on sales_fact_1997 for each row set new.last_update = unix_timestamp(now());
-create trigger sales_fact_1998_insert before insert on sales_fact_1998 for each row set new.last_update = unix_timestamp(now());
-create trigger sales_fact_1998_update before update on sales_fact_1998 for each row set new.last_update = unix_timestamp(now());
-create trigger sales_fact_dec_1998_insert before insert on sales_fact_dec_1998 for each row set new.last_update = unix_timestamp(now());
-create trigger sales_fact_dec_1998_update before update on sales_fact_dec_1998 for each row set new.last_update = unix_timestamp(now());
-create trigger promotion_insert before insert on promotion for each row set new.last_update = unix_timestamp(now());
-create trigger promotion_update before update on promotion for each row set new.last_update = unix_timestamp(now());
-create trigger store_insert before insert on store for each row set new.last_update = unix_timestamp(now());
-create trigger store_update before update on store for each row set new.last_update = unix_timestamp(now());
-create trigger time_by_day_insert before insert on time_by_day for each row set new.last_update = unix_timestamp(now());
-create trigger time_by_day_update before update on time_by_day for each row set new.last_update = unix_timestamp(now());
+alter table $table add last_update INT;
+update $table set last_update = UNIX_TIMESTAMP(NOW());
+create trigger insert_$table before insert on $table for each row set new.last_update = unix_timestamp(now());
+create trigger update_$table before update on $table for each row set new.last_update = unix_timestamp(now());
 EOF
-clear
+done
